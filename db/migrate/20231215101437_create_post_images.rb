a571fcac68617c1ce7932eac5157b3158e64d464
EC2 Default User <149554348+AoKeito@users.noreply.github.com>
2023-12-17 08:21:57 +0000

    完成

--- db/migrate/20231215101437_create_post_images.rb
+++ db/migrate/20231215101437_create_post_images.rb
@@ -1,7 +1,6 @@
 class CreatePostImages < ActiveRecord::Migration[6.1]
   def change
-    create_table :post_images, :id => false do |t|
-      t.integer :id, :primary_key => true
+    create_table :post_images do |t|
       t.integer :user_id
       t.string :shop_name
       t.text :caption
